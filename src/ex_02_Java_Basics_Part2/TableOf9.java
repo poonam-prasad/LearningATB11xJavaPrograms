e99eeba35fcce4847cbb54398a9485664b4f74e4
poonam-prasad <poonam.prasad@hcl-software.com>
2025-03-11 23:07:13 +0530

    Pushing TableOf9.java program using printf

---
+++ src/ex_02_Java_Basics_Part2/TableOf9.java
@@ -0,0 +1,29 @@
+package ex_02_Java_Basics_Part2;
+
+public class TableOf9 {
+
+    //Printing Table of 9 using printf
+
+    public static void main(String[] args) {
+        System.out.printf("%d*%d=%d",9,1,9);
+        System.out.println();
+        System.out.printf("%d*%d=%d",9,2,18);
+        System.out.println();
+        System.out.printf("%d*%d=%d",9,3,27);
+        System.out.println();
+        System.out.printf("%d*%d=%d",9,4,36);
+        System.out.println();
+        System.out.printf("%d*%d=%d",9,5,45);
+        System.out.println();
+        System.out.printf("%d*%d=%d",9,6,54);
+        System.out.println();
+        System.out.printf("%d*%d=%d",9,7,63);
+        System.out.println();
+        System.out.printf("%d*%d=%d",9,8,72);
+        System.out.println();
+        System.out.printf("%d*%d=%d",9,9,81);
+        System.out.println();
+        System.out.printf("%d*%d=%d",9,10,90);
+
+    }
+}
