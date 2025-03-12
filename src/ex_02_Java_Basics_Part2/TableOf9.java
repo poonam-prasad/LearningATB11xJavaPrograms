c59e589a623d78738b00a8038e8dc6ba9d9b16c8
poonam-prasad <poonam.prasad@hcl-software.com>
2025-03-12 08:23:54 +0530

    Pushing Updated TableOf9.java program using printf

--- src/ex_02_Java_Basics_Part2/TableOf9.java
+++ src/ex_02_Java_Basics_Part2/TableOf9.java
@@ -5,25 +5,17 @@ public class TableOf9 {
     //Printing Table of 9 using printf
 
     public static void main(String[] args) {
-        System.out.printf("%d*%d=%d",9,1,9);
-        System.out.println();
-        System.out.printf("%d*%d=%d",9,2,18);
-        System.out.println();
-        System.out.printf("%d*%d=%d",9,3,27);
-        System.out.println();
-        System.out.printf("%d*%d=%d",9,4,36);
-        System.out.println();
-        System.out.printf("%d*%d=%d",9,5,45);
-        System.out.println();
-        System.out.printf("%d*%d=%d",9,6,54);
-        System.out.println();
-        System.out.printf("%d*%d=%d",9,7,63);
-        System.out.println();
-        System.out.printf("%d*%d=%d",9,8,72);
-        System.out.println();
-        System.out.printf("%d*%d=%d",9,9,81);
-        System.out.println();
-        System.out.printf("%d*%d=%d",9,10,90);
+        int num=9;
+        System.out.printf("%d*1=%d\n",num,num);
+        System.out.printf("%d*2=%d\n",num,num*2);
+        System.out.printf("%d*3=%d\n",num,num*3);
+        System.out.printf("%d*4=%d\n",num,num*4);
+        System.out.printf("%d*5=%d\n",num,num*5);
+        System.out.printf("%d*6=%d\n",num,num*6);
+        System.out.printf("%d*7=%d\n",num,num*7);
+        System.out.printf("%d*8=%d\n",num,num*8);
+        System.out.printf("%d*9=%d\n",num,num*9);
+        System.out.printf("%d*10=%d",num,num*10);
 
     }
 }
