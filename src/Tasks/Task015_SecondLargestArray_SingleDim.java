65faafb2ba22832ff62e573ef619ff1274541c36
poonam-prasad <poonam.prasad@hcl-software.com>
2025-03-31 07:00:13 +0530

    Pushing Tasks & Programs completed on 31st March

--- src/Tasks/Task015_SecondLargestArray_SingleDim.java
+++ src/Tasks/Task015_SecondLargestArray_SingleDim.java
@@ -1,13 +1,13 @@
 package Tasks;
 
-public class Task_SecondLargestArray_IfElse {
+public class Task015_SecondLargestArray_SingleDim {
     public static void main(String[] args) {
         int max=0;
         int second_largest=0;
 
         int[] numbers={10,30,20,50,40};
 
-       for(int i=0;i< numbers.length-1;i++)
+       for(int i=0;i< numbers.length;i++)
        {
            if(numbers[i]>max)
            {
@@ -18,8 +18,11 @@ public class Task_SecondLargestArray_IfElse {
            else if(second_largest>numbers[i] && numbers[i]!=max)
            {
                 second_largest=numbers[i];
+
            }
        }
+        System.out.println("Max number is "+max);
+        System.out.println("Second largest no is "+second_largest);
 
     }
 }
